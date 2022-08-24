cefceec9dea37159e3d3616e3f82709adb8841ea
Mikayel Saghyan <xxmuaddib@gmail.com>
2022-08-24 14:45:53 +0400

    Code cleanup

--- src/App/components/Game/components/DigitsStatuses/DigitsStatuses.js
+++ src/App/components/Game/components/DigitsStatuses/DigitsStatuses.js
@@ -2,25 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 import cn from 'classnames';
 
-export const DigitsStatuses = ({ input, secretCode }) => {
-  return (
-    <DigitsStatusesContainer>
-      {input.split('').map((digit, ind) => (
-        <DigitStatus
-          key={`${digit}-${ind}`}
-          className={cn(
-            `animate__animated ${
-              secretCode[ind] === digit ? 'animate__tada' : 'animate__wobble'
-            }`
-          )}
-          isCorrectAnswer={secretCode[ind] === digit}
-        >
-          {secretCode[ind] === digit ? 'V' : 'X'}
-        </DigitStatus>
-      ))}
-    </DigitsStatusesContainer>
-  );
-};
+export const DigitsStatuses = ({ input, secretCode }) => (
+  <DigitsStatusesContainer>
+    {input.split('').map((digit, ind) => (
+      <DigitStatus
+        key={`${digit}-${ind}`}
+        className={cn(
+          `animate__animated ${
+            secretCode[ind] === digit ? 'animate__tada' : 'animate__wobble'
+          }`
+        )}
+        isCorrectAnswer={secretCode[ind] === digit}
+      >
+        {secretCode[ind] === digit ? 'V' : 'X'}
+      </DigitStatus>
+    ))}
+  </DigitsStatusesContainer>
+);
 
 const DigitsStatusesContainer = styled.div`
   display: flex;
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 import { App } from './App';
 import { store } from './redux/store';
 
-const root = ReactDOM.render(
+ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
