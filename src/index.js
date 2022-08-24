2d7a54d2c55d697121f7f18bb8c5334f4bed7bac
Mikayel Saghyan <xxmuaddib@gmail.com>
2022-08-24 14:42:49 +0400

    Fix build problem

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import './index.css';
 import { App } from './App';
 import { store } from './redux/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
+  document.getElementById('root')
 );
